d94adcf51357dfc25ab154138dcdceaf032d2999
AloneCoding <jom.karunpat@alonecoding.com>
2022-04-23 12:35:05 +0700

    Change name of route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
+import Home from "../views/Home.vue";
 const history = createWebHistory();
 const routes = [
     {
         path: "/",
-        name: "Dashboard",
-        component: Dashboard,
+        name: "Home",
+        component: Home,
     },
 ];
 const router = createRouter({ history, routes });
