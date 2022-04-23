5efcf8d949deed566b77d3601cbcd358318dbefa
AloneCoding <jom.karunpat@alonecoding.com>
2022-04-23 12:34:58 +0700

    Add Event dialog

---
+++ src/components/AddEventDialog.vue
@@ -0,0 +1,87 @@
+<script setup>
+const props = defineProps({
+    openEventModal: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <div
+        style="background-color: rgba(0, 0, 0, 0.8)"
+        class="fixed z-40 top-0 right-0 left-0 bottom-0 h-full w-full"
+        v-show.transition.opacity="openEventModal"
+    >
+        <div
+            class="p-4 max-w-xl mx-auto relative absolute left-0 right-0 overflow-hidden mt-24"
+        >
+            <div
+                class="shadow absolute right-0 top-0 w-10 h-10 rounded-full bg-white text-gray-500 hover:text-gray-800 inline-flex items-center justify-center cursor-pointer"
+                @:click="openEventModal = !openEventModal"
+            >
+                <svg
+                    class="fill-current w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        d="M16.192 6.344L11.949 10.586 7.707 6.344 6.293 7.758 10.535 12 6.293 16.242 7.707 17.656 11.949 13.414 16.192 17.656 17.606 16.242 13.364 12 17.606 7.758z"
+                    />
+                </svg>
+            </div>
+
+            <div
+                class="shadow w-full rounded-lg bg-white overflow-hidden w-full block p-8"
+            >
+                <h2 class="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">
+                    เพิ่ม Event
+                </h2>
+
+                <div class="mb-4">
+                    <label
+                        class="text-gray-800 block mb-1 font-bold text-sm tracking-wide"
+                        >ชื่อกิจกรรม</label
+                    >
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                        type="text"
+                        v-model="event_title"
+                    />
+                </div>
+
+                <div class="mb-4">
+                    <label
+                        class="text-gray-800 block mb-1 font-bold text-sm tracking-wide"
+                        >อีเมล</label
+                    >
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                        type="text"
+                        v-model="event_date"
+                        readonly
+                    />
+                </div>
+
+                <div class="mt-8 text-right">
+                    <button
+                        type="button"
+                        class="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-300 rounded-lg shadow-sm mr-2"
+                        @click="openEventModal = !openEventModal"
+                    >
+                        ยกเลิก
+                    </button>
+                    <button
+                        type="button"
+                        class="bg-green-700 hover:bg-green-800 text-white font-semibold py-2 px-4 border border-green-700 rounded-lg shadow-sm"
+                        @click="addEvent()"
+                    >
+                        บันทึก
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
