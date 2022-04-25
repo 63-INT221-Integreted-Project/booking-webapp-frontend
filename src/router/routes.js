8e03a3e88dffd3be9a25b4be977213a64f0848e0
AloneCoding <jom.karunpat@alonecoding.com>
2022-04-25 14:05:09 +0700

    Add Route support

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import EventCategories from "../views/EventCategories.vue";
 const history = createWebHistory();
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home,
+    },
+    {
+        path: "/event-categories",
+        name: "EventCategories",
+        component: EventCategories,
     },
 ];
 const router = createRouter({ history, routes });
