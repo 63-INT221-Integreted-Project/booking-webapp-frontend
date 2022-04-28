84ddefcabe9c984bba0c0adca7cb199e613365ce
AloneCoding <jom.karunpat@alonecoding.com>
2022-04-28 17:00:13 +0700

    Add post and patch

--- src/services/event-categories.service.js
+++ src/services/event-categories.service.js
@@ -6,6 +6,40 @@ const findAll = async function () {
     );
 };
 
+const createEventCategory = async function (eventCategory) {
+    return await fetch(`${BaseUrl.getUrl()}/event-categories/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            eventCategoryName: eventCategory.eventCategoryName,
+            eventCategoryDescription: eventCategory.eventCategoryDescription,
+            eventDuration: eventCategory.eventDuration,
+        }),
+    }).then((res) => res.json());
+};
+
+const updateEventCategory = async function (eventCategoryId, eventCategory) {
+    return await fetch(
+        `${BaseUrl.getUrl()}/event-categories/${eventCategoryId}`,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                eventCategoryName: eventCategory.eventCategoryName,
+                eventCategoryDescription:
+                    eventCategory.eventCategoryDescription,
+                eventDuration: eventCategory.eventDuration,
+            }),
+        }
+    ).then((res) => res.json());
+};
+
 export default {
     findAll,
+    createEventCategory,
+    updateEventCategory,
 };
