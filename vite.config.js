d1c4a04cdf95b5e730fafe30e1fb67aac5ae3dc1
AloneCoding <jom.karunpat@alonecoding.com>
2022-05-01 18:50:06 +0700

    Change hostname in vite

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         port: 3000,
         proxy: {
-            "/api": "http://localhost:8080",
+            "/api": "http://oasip-backend:8080",
         },
     },
 });
