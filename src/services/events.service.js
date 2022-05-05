5137b2a27ce369e88891429c48ca898674920f7c
AloneCoding <jom.karunpat@alonecoding.com>
2022-05-05 14:51:45 +0700

    Remove ununsed function

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -1,23 +1,5 @@
 import BaseUrl from "../utils/BaseUrl";
 
-const addEvent = async function (event) {
-    return await fetch(`${BaseUrl.getUrl()}/events`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            bookingName: event.bookingName,
-            bookingEmail: event.bookingEmail,
-            eventStartTime: event.eventStartTime,
-            eventNotes: event.eventNotes,
-            eventCategoryId: {
-                eventCategoryId: event.eventCategoryId,
-            },
-        }),
-    }).then((res) => res.json());
-};
-
 const findAllByBetweenDate = async function (date1, date2) {
     return await fetch(
         `${BaseUrl.getUrl()}/events/check-between?date1=${date1}&date2=${date2}`
@@ -42,7 +24,7 @@ const createEvent = async function (event) {
             eventStartTime: event.eventStartTime,
             eventDuration: +event.eventDuration,
             eventNotes: event.eventNotes,
-            eventCategory: event.eventCategory,
+            eventCategoryId: event.eventCategoryId,
         }),
     });
 };
@@ -55,7 +37,6 @@ const cancleEvent = async function (eventId) {
 
 export default {
     search,
-    addEvent,
     findAllByBetweenDate,
     createEvent,
     cancleEvent,
