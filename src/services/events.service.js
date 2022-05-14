c0949933cda499e4be3491a6284dec27b029923a
AloneCoding <jom.karunpat@alonecoding.com>
2022-05-14 16:15:49 +0700

    Add when not select dateStart and dateEnd need to send to backend but it can be empty string

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -24,8 +24,8 @@ const search = async function (dateStart, dateEnd, category, word) {
     let queryString = {};
     if (category) queryString.category = category;
     if (word) queryString.word = word;
-    if (dateStart !== "Invalid Date") queryString.dateStart = dateStart;
-    if (dateEnd !== "Invalid Date") queryString.dateEnd = dateEnd;
+    queryString.dateStart = dateStart !== "Invalid Date" ? dateStart : "";
+    queryString.dateEnd = dateEnd !== "Invalid Date" ? dateEnd : "";
     let url =
         "/search?" +
         Object.entries(queryString)
