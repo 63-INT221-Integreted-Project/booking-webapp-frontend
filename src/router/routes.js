b91c461225ef183719be2cba4d988ce47720ecbb
AloneCoding <jom.karunpat@alonecoding.com>
2022-05-17 13:08:52 +0700

    Add route meta and title

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,16 +8,55 @@ const routes = [
         path: "/",
         name: "Home",
         component: Home,
+        meta: {
+            title: "OASIP - Booking",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "Index booking of OASIP.",
+                },
+                {
+                    property: "og:description",
+                    content: "Index booking of OASIP.",
+                },
+            ],
+        },
     },
     {
         path: "/event-categories",
         name: "EventCategories",
         component: EventCategories,
+        meta: {
+            title: "OASIP - Event Category",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "Event category of OASIP.",
+                },
+                {
+                    property: "og:description",
+                    content: "Event category of OASIP.",
+                },
+            ],
+        },
     },
     {
         path: "/teams",
         name: "Teams",
         component: Teams,
+        meta: {
+            title: "OASIP - Teams",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "Teams create of OASIP.",
+                },
+                {
+                    property: "og:description",
+                    content: "Teams create of OASIP.",
+                },
+            ],
+        },
     },
 ];
 const router = createRouter({ history, routes });
