9eb8627af2085f05631073c31d1aa900c9fa0b3f
AloneCoding <jom.karunpat@alonecoding.com>
2022-08-22 22:44:30 +0700

    change: base url

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
-    // base: "/kp2/",
+    base: "/kp2/",
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
     },
